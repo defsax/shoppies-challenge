b8a61897b109b7296089203d1f7450defe84e3af
Per <defayetteperry@gmail.com>
2021-05-06 00:52:42 -0400

    Add nominationItem rename.

---
+++ src/components/nominationItem.jsx
@@ -0,0 +1,20 @@
+export default function NominationItem(props) {
+  const { nomination } = props;
+  console.log(props);
+  return (
+    <div>
+      <p>
+        {nomination.Title} ({nomination.Year})
+      </p>
+      <img src={nomination.Poster} alt={nomination.Title}></img>
+      <button
+      // onClick={() => props.onClick(nomination)}
+      // disabled={props.isNominated(nomination.imdbID)}
+      >
+        Un-Nominate
+      </button>
+      <br />
+      <br />
+    </div>
+  );
+}
