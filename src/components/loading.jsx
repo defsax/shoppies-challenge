fcabab5d3e530cc858a6fc9198acfdf6143af426
Per <defayetteperry@gmail.com>
2021-05-08 02:32:05 -0400

    Add loader image in.

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -3,7 +3,12 @@ import '../style/css/loading.css';
 export default function Loading() {
   return (
     <div className="rotate linear infinite loading">
-      <p>loading...</p>
+      <img
+        src={
+          'https://raw.githubusercontent.com/defsax/shoppies-challenge/master/public/resources/imgs/loading.png'
+        }
+        alt={'Loading...'}
+      ></img>
     </div>
   );
 }
